816de3de9dc138b85b02ec7e7ccd3ff0f9db2973
pis0sion <zihuang2010@126.com>
2026-05-10 17:38:10 +0800

    chore(chathub-proto): rerun-if-changed on proto dir + bytes workspace dep
    - build.rs 增加 dir-level rerun-if-changed,新增 .proto 文件能触发重 build
    - bytes 从 chathub-proto crate 内联升级到 workspace.dependencies 统一管理

--- crates/chathub-proto/build.rs
+++ crates/chathub-proto/build.rs
@@ -21,6 +21,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     ];
 
     println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed={}", proto_root.display());
     for f in &proto_files {
         println!("cargo:rerun-if-changed={}", f.display());
     }
